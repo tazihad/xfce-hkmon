191eb0a44168ae4d6620660bc05a9e7d6e67b907
Tarek <tazihad@gmail.com>
2021-04-18 22:31:32 +0600

    Update xfce-hkmon.cpp

--- xfce-hkmon.cpp
+++ xfce-hkmon.cpp
@@ -398,7 +398,7 @@ std::istream& operator>>(std::istream& in, Network::Interface& ifz)
 std::ostream& operator<<(std::ostream& out, const Network::Bandwidth& speed)
 {
     char unit = (speed.unit == Network::Bandwidth::Unit::byte)? 'b' : 'B';
-    if (speed.perSecond < MB_i) return out << (speed.perSecond / 1000)/8 << " K" << unit << "ps";
+    if (speed.perSecond < 10240000) return out << (speed.perSecond / 1024)/8 << " K" << unit << "ps";
     return out << std::fixed << std::setprecision(2) << (speed.perSecond / MB_f)/8 << " M" << unit << "ps";
 }
 
